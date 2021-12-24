66632b8798b280fb22f86480960c6a68371692d4
Naseem khan <53051431+megamindnk@users.noreply.github.com>
2021-12-24 11:27:33 +0530

    some more changes

--- src/pages/auth/Logo.tsx
+++ src/pages/auth/Logo.tsx
@@ -4,7 +4,7 @@ import LogoImage from './logo.png'
 const Logo: React.FC = () => {
   return (
     <div style={{display:"flex",alignContent:"center",justifyContent:'center',marginTop:50}}>
-    <img src={LogoImage} alt="Docuclip" width={120} />
+    <img src={LogoImage} alt="Docuclip" width={120} height={60} /> 
     </div>
   )
 }
